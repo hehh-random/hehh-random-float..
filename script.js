a72702485826e4f5c02c8cc7ff216fab73e21bc0
hehh-random <jiyabhandari767@gmail.com>
2026-03-15 18:42:54 +1300

    time to 200

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ function createHeart() {
   document.body.appendChild(heart);
   setTimeout(() => heart.remove(), 10000); // Remove after animation
 }
-setInterval(createHeart, 100); // Make a new heart every 100ms
+setInterval(createHeart, 200); // Make a new heart every 200ms
